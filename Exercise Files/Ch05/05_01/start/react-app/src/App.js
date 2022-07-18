d8f1894b1e7b426a2b94c622bd8d794d550eaaaa
David Taing <66336324+dat-monke@users.noreply.github.com>
2022-07-17 22:13:54 -0700

    shorthand, creating notion notes, etc

--- Exercise Files/Ch05/05_01/start/react-app/src/App.js
+++ Exercise Files/Ch05/05_01/start/react-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useReducer, useState } from "react";
+import { useReducer, useState, useRef } from "react";
 
 function useInput(initialValue) {
   const [value, setValue] = useState(initialValue)
@@ -9,21 +9,37 @@ function useInput(initialValue) {
 }
 
 function App() {
+  const txtTitle = useRef()
+  const hexColor = useRef()
+
+  // Take down the current value and create an alert box
+  const submit = e => {
+    e.preventDefault()
+    // const title = txtTitle.current.value
+    // const color = hexColor.current.value
+    alert(`${title}, ${color}`)
+    txtTitle.current.value=""
+    hexColor.current.value=""
+  }
+
   const [checked, setChecked] = useReducer(
     (checked) => !checked,
     false
   );
   return (
-    <div className="App">
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={setChecked}
-      />
-      <label>
-        {checked ? "checked" : "not checked"}
-      </label>
-    </div>
+    <form onSubmit={submit}>
+      <input ref={txtTitle} type="text" placeholder="color title..."/>
+    </form>
+    // <div className="App">
+    //   <input
+    //     type="checkbox"
+    //     value={checked}
+    //     onChange={setChecked}
+    //   />
+    //   <label>
+    //     {checked ? "checked" : "not checked"}
+    //   </label>
+    // </div>
   );
 }
 
